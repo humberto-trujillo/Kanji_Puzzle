08e07cf3e351d6b2440bf608227cf35bb3653af2
Tito <htp.tito@gmail.com>
2016-12-18 23:19:47 -0800

    The Game Board

---
+++ Kanji_Puzzle/Assets/Scripts/Board.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour
+{
+	public int width;
+	public int height;
+	public GameObject tilePrefab;
+	Tile[,] m_allTiles;
+
+	void Start ()
+	{
+		m_allTiles = new Tile[width,height];
+		SetupTiles();
+	}
+
+	void SetupTiles()
+	{
+		for (int i = 0; i < width; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				GameObject tile = Instantiate(tilePrefab,new Vector3(i,j,0),Quaternion.identity) as GameObject;
+				tile.name = "Tile( "+i+" , "+j+")";
+				m_allTiles[i,j] = tile.GetComponent<Tile>();
+				tile.transform.parent = transform;
+			}
+		}
+	}
+}
