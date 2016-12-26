54a7dc6f55c21fb4942830f8ce38170d727176e8
Tito <htp.tito@gmail.com>
2016-12-25 22:34:27 -0800

    Collapsing Columns

--- Kanji_Puzzle/Assets/Scripts/Board.cs
+++ Kanji_Puzzle/Assets/Scripts/Board.cs
@@ -188,6 +188,9 @@ public class Board : MonoBehaviour {
 				yield return new WaitForSeconds(swapTime);
 				ClearPieceAt (clickedPieceMatches);
 				ClearPieceAt (targetPieceMatches);
+
+				CollapseColumn(clickedPieceMatches);
+				CollapseColumn(targetPieceMatches);
 				//HighlightMatchesAt(clickedTile.xIndex,clickedTile.yIndex);
 				//HighlightMatchesAt(targetTile.xIndex,targetTile.yIndex);
 			}
@@ -418,5 +421,56 @@ public class Board : MonoBehaviour {
 			ClearPieceAt (piece.xIndex, piece.yIndex);
 			FillRandomAt (piece.xIndex, piece.yIndex);
 		}
+	}
+
+	List<GamePiece> CollapseColumn(int column, float collapseTime = 0.1f)
+	{
+		List<GamePiece> movingPieces = new List<GamePiece>();
+		for (int i = 0; i < height - 1; i++)
+		{
+			if(m_allGamePieces[column,i] == null)
+			{
+				for (int j = i + 1; j < height; j++)
+				{
+					if(m_allGamePieces[column,j] != null)
+					{
+						m_allGamePieces[column,j].Move(column,i,collapseTime);
+						m_allGamePieces[column,i] = m_allGamePieces[column,j];
+						m_allGamePieces[column,i].SetCoord(column,i);
+						if(!movingPieces.Contains(m_allGamePieces[column,i]))
+						{
+							movingPieces.Add(m_allGamePieces[column,i]);
+						}
+						m_allGamePieces[column,j] = null;
+						break;
+					}	
+				}
+			}	
+		}
+		return movingPieces;
+	}
+
+	List<GamePiece> CollapseColumn(List<GamePiece> gamePieces)
+	{
+		List<GamePiece> movingPieces = new List<GamePiece>();
+		List<int> columnsToCollapse = GetColumns(gamePieces);
+		foreach (int column in columnsToCollapse) 
+		{
+			movingPieces = movingPieces.Union(CollapseColumn(column)).ToList();	
+		}
+		return movingPieces;
+	}
+
+	List<int> GetColumns(List<GamePiece> gamePieces)
+	{
+		List<int> columns = new List<int>();
+		foreach (var piece in gamePieces)
+		{
+			if(!columns.Contains(piece.xIndex))
+			{
+				columns.Add(piece.xIndex);
+			}	
+		}
+		return columns;
 	}
 }
